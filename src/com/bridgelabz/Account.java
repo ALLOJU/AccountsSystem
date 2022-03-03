3309f4a0503a233c6555122aabf161b0154fd8ab
ALLOJU <mounika.alloju@gmail.com>
2022-03-03 09:28:23 +0530

    added accounts with debit amount

--- src/com/bridgelabz/Account.java
+++ src/com/bridgelabz/Account.java
@@ -1,28 +1,31 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Account {
-
-	private double balance;
-
-	//created parametrized constructer
-	public Account(double initialbalance) {
-		if(initialbalance > 0.0)
-			balance=initialbalance;
+    private double balance;
+    
+	public Account(double balance) {
+	this.balance = balance;
 	}
-	//method to credit amount to account
-	public void credit(double amount)
-	{
-		balance =balance + amount;
-	}
-	//method to debit amount to account
-	public void debit(double amount)
-	{
-		balance =balance - amount;
-	}
-	public double getBalance() {
-		return balance;
-	}
+    public void debit(double balance) {
+     System.out.println("please enter the amount to withdraw");
+     Scanner sc = new Scanner(System.in);
+     double withdraw = sc.nextInt();
+     if(withdraw>balance) {
+    	 System.out.println("withdraw amount is greater than balance");
+     }
+     else {
+    	 balance=balance -withdraw;
+    	 System.out.println("balance is  "+balance);
+     }
+     
+    }
 
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the balance");
+		Account account = new Account(sc.nextDouble());
+		account.debit(account.balance);
+	}
 }
-
